7a2bb07babea81da5c71cd9e9ff8b30eb0e5fed8
Dishant Rathi <dishant16rathi@gmail.com>
2021-05-28 16:00:56 +0530

    Cat and Dogs

---
+++ catAndDogs.py
@@ -0,0 +1,15 @@
+# cook your dish here
+try:
+    t=int(input())
+    for _ in range(t):
+        c,d,l=map(int,input().split())
+        max=(c+d)*4
+        min=d*4
+        if c>2*d:
+            min=max-2*min
+        if l>max or l<min or l%4!=0:
+            print("no")
+        else:
+            print("yes")
+except:
+    pass
